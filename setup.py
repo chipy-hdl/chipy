bcac6a4b0e7454564eb9b687416d7380f2ec0611
David Craven <david@craven.ch>
2018-03-31 14:10:26 +0200

    Add setup.py and MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='chipy',
+    packages=find_packages(exclude=['tests']),
+    version='0.0.1',
+    description='Chipy is a single-file python module for generating digital hardware.',
+    long_description=open('README.md').read(),
+    author='Clifford Wolf',
+    author_email='clifford@clifford.at',
+    url='https://github.com/chipy-hdl/chipy',
+    keywords=['eda', 'cad', 'hdl', 'verilog'],
+    license='ISC'
+)
